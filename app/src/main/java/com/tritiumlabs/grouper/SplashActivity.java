6433eb05173676a48e5df4d1d0dd8ab824d5149d
Kdowding78 <kdowding78@hotmail.com>
2016-11-20 06:45:34 -0500

    added signup layout, modified buttons

--- app/src/main/java/com/tritiumlabs/grouper/SplashActivity.java
+++ app/src/main/java/com/tritiumlabs/grouper/SplashActivity.java
@@ -11,8 +11,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //ProgressBar progressBar = (ProgressBar) findViewById(R.id.activity_progress_bar);
-
         Intent intent = new Intent(this, StarterActivity.class);
         startActivity(intent);
         finish();
