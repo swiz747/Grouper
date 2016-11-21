cf3852f49f5497a4bec6ee1a702854b6ca003f57
Kdowding78 <kdowding78@hotmail.com>
2016-11-20 21:14:45 -0500

    layout changes

--- app/src/main/java/com/tritiumlabs/grouper/SettingsActivity.java
+++ app/src/main/java/com/tritiumlabs/grouper/SettingsActivity.java
@@ -1,18 +1,15 @@
 package com.tritiumlabs.grouper;
 
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.StateListDrawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import fragments.settingsfragments.AccountFragment;
+import fragments.settingsfragments.DefaultFragment;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -22,20 +19,15 @@ public class SettingsActivity extends AppCompatActivity {
     Button btnThemes;
     Button btnNotifications;
 
-    AnimationDrawable fishBlinkAnimation;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
+        openDefaultFragment();
+
         //Objects
         btnAccount = (Button)findViewById(R.id.btnAccount);
-
-
-        ImageView fishImage = (ImageView) findViewById(R.id.imgFishBlink);
-        fishImage.setBackgroundResource(R.drawable.fish_blink_animation);
-        fishBlinkAnimation = (AnimationDrawable) fishImage.getBackground();
-        fishBlinkAnimation.start();
 
         //Listeners
         btnAccount.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +36,12 @@ public class SettingsActivity extends AppCompatActivity {
                 openAccountFragment();
             }
         });
+    }
+
+    public void openDefaultFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragContainerSettings, new DefaultFragment()).addToBackStack("settingsDefaultFragment").commit();
     }
 
     public void openAccountFragment() {
