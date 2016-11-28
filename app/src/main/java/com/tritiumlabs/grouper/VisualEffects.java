a3cacdf78fc664d66c28b0a752f4f20c621ef1be
Kdowding78 <kdowding78@hotmail.com>
2016-11-28 10:53:16 -0500

    few settings changes, also added a visual effects class which will be used to store some reusable code for transition effects

---
+++ app/src/main/java/com/tritiumlabs/grouper/VisualEffects.java
@@ -0,0 +1,39 @@
+package com.tritiumlabs.grouper;
+
+import android.content.Context;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+/**
+ * Created by Kyle on 11/27/2016.
+ */
+
+public class VisualEffects {
+
+    public static void slide_down(Context ctx, View v){
+
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_down);
+        if(a != null){
+            a.reset();
+            if(v != null){
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
+    }
+
+    public static void slide_up(Context ctx, View v){
+
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_up);
+        if(a != null){
+            a.reset();
+            if(v != null){
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
+    }
+}
+
+
--- app/src/main/java/fragments/settingsfragments/SecurityFragment.java
+++ app/src/main/java/fragments/settingsfragments/SecurityFragment.java
@@ -1,5 +1,6 @@
 package fragments.settingsfragments;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,19 +8,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ExpandableListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.vision.text.Text;
 import com.tritiumlabs.grouper.R;
+import com.tritiumlabs.grouper.VisualEffects;
 
 public class SecurityFragment extends Fragment {
 
     private static final String TAG = "SecurityFragment(Settings)";
 
     TextView txtSecurity;
-    RelativeLayout rlayPrivacy;
     TextView txtViewingPrivacy;
+    TextView txtFriendRequest;
+    RelativeLayout rlayPrivacy;
+    RelativeLayout rlayFriendRequest;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_security_layout, container, false);
@@ -28,25 +35,43 @@ public class SecurityFragment extends Fragment {
 
         txtSecurity = (TextView) view.findViewById(R.id.txtSettingsSecurity);
         txtViewingPrivacy = (TextView) view.findViewById(R.id.settingsSecurityViewingPrivacy);
+        txtFriendRequest = (TextView) view.findViewById(R.id.settingsSecurityFriendRequest);
         rlayPrivacy = (RelativeLayout) view.findViewById(R.id.rlaySettingsSecurityViewingPrivacy);
+        rlayFriendRequest = (RelativeLayout) view.findViewById(R.id.rlaySettingsSecurityFriendRequest);
 
         txtSecurity.setTypeface(sab);
         txtViewingPrivacy.setTypeface(sab);
+        txtFriendRequest.setTypeface(sab);
 
         rlayPrivacy.setVisibility(View.GONE);
+        rlayFriendRequest.setVisibility(View.GONE);
 
         txtViewingPrivacy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                toggle_contents();
+                toggleContentsPrivacy();
             }
         });
+
+        txtFriendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleContentsFriendRequest();
+            }
+        });
+
         return view;
     }
 
-    public void toggle_contents(){
+    public void toggleContentsPrivacy(){
         rlayPrivacy.setVisibility( rlayPrivacy.isShown()
-                        ? View.GONE
-                        : View.VISIBLE );
+                ? View.GONE
+                : View.VISIBLE );
+    }
+
+    public void toggleContentsFriendRequest(){
+        rlayFriendRequest.setVisibility( rlayFriendRequest.isShown()
+                ? View.GONE
+                : View.VISIBLE );
     }
 }
